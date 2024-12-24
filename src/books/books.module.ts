f3a1c153420b0c14d74987b18fe994c7c90203b0
Anass-Dr <anasdrissi77@gmail.com>
2024-12-24 12:08:45 +0100

    feat: Add users module to books module imports

--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { BookLoan, BookLoanSchema } from './schemas/book-loan.schema';
 import { Book, BookSchema } from './schemas/book.schema';
 import { MailModule } from 'src/mail/mail.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { MailModule } from 'src/mail/mail.module';
       { name: BookLoan.name, schema: BookLoanSchema },
     ]),
     MailModule,
+    UsersModule,
   ],
   controllers: [BooksController],
   providers: [BooksService],
